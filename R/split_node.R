5846f8bbfc48d82fa39b271196f8db099ed79f28
holgstr <hbj.loewe@gmail.com>
2024-03-01 05:28:35 +0100

    more careful covariate processing

--- R/split_node.R
+++ R/split_node.R
@@ -47,8 +47,7 @@ eval_split_cand_categorical <- function(split_cand, covariate, valid_set, alpha)
 #' @keywords internal
 #'
 eval_split_cand <- function(ids_left, valid_set, alpha) {
-  ids_candidates_left_valid_set <- ids_left[ids_left %in% valid_set$testing_ids]
-  n_left <- length(ids_candidates_left_valid_set)
+  n_left <- length(ids_left)
   n_min <- min(n_left, nrow(valid_set) - n_left)
   alpha >= 1 / (n_min + 1)
 }
@@ -58,7 +57,7 @@ eval_split_cand <- function(ids_left, valid_set, alpha) {
 #' @param node_id (`count`)\cr parent node identifier.
 #' @param var_name (`string`)\cr name of the feature to be split.
 #' @param split_cand (`number`)\cr value of the split point.
-#' @param covariate (`numeric`)\cr covariate values of the observations in the parent node.
+#' @param x_data (`data.frame`)\cr feature data matrix of the parent node.
 #' @param valid_set (`data.frame`)\cr validation set.
 #'   See [get_valid_set()] for details.
 #' @param alpha (`proportion`)\cr miscoverage rate.
@@ -66,9 +65,9 @@ eval_split_cand <- function(ids_left, valid_set, alpha) {
 #' @return List with elements `node_id`, `feature`, `feature_type`, `split_cand`, `ids_left_child`, `ids_right_child` and `conf_crit`.
 #' @keywords internal
 #'
-process_split_config_numeric <- function(node_id, var_name, split_cand, covariate, valid_set, alpha, lambda) {
-  ids_candidates_left <- which(covariate < split_cand)
-  ids_candidates_right <- which(covariate >= split_cand)
+process_split_config_numeric <- function(node_id, var_name, split_cand, x_data, valid_set, alpha, lambda) {
+  ids_candidates_left <- which((1:nrow(x_data) %in% valid_set$testing_ids) & x_data[,var_name] < split_cand)
+  ids_candidates_right <- which((1:nrow(x_data) %in% valid_set$testing_ids) & x_data[,var_name] >= split_cand)
   conf_crit <- process_split_config(ids_candidates_left, ids_candidates_right, valid_set, alpha, lambda)
   list("node_id" = node_id, "feature" = var_name, "feature_type" = "numeric", "split_cand" = split_cand, "ids_left_child" = ids_candidates_left, "ids_right_child" = ids_candidates_right, "conf_crit" = conf_crit)
 }
@@ -78,7 +77,7 @@ process_split_config_numeric <- function(node_id, var_name, split_cand, covariat
 #' @param node_id (`count`)\cr parent node identifier.
 #' @param var_name (`string`)\cr name of the feature to be split.
 #' @param split_cand (`character`)\cr split point, set of categories representing the left child node.
-#' @param covariate (`factor`)\cr covariate values of the observations in the parent node.
+#' @param x_data (`data.frame`)\cr feature data matrix of the parent node.
 #' @param valid_set (`data.frame`)\cr validation set.
 #'   See [get_valid_set()] for details.
 #' @param alpha (`proportion`)\cr miscoverage rate.
@@ -86,9 +85,9 @@ process_split_config_numeric <- function(node_id, var_name, split_cand, covariat
 #' @return List with elements `node_id`, `feature`, `feature_type`, `split_cand`, `ids_left_child`, `ids_right_child` and `conf_crit`.
 #' @keywords internal
 #'
-process_split_config_categorical <- function(node_id, var_name, split_cand, covariate, valid_set, alpha, lambda) {
-  ids_candidates_left <- which(covariate %in% split_cand)
-  ids_candidates_right <- which(!(covariate %in% split_cand))
+process_split_config_categorical <- function(node_id, var_name, split_cand, x_data, valid_set, alpha, lambda) {
+  ids_candidates_left <- which((1:nrow(x_data) %in% valid_set$testing_ids) & x_data[,var_name] %in% split_cand)
+  ids_candidates_right <- which((1:nrow(x_data) %in% valid_set$testing_ids) & !(x_data[,var_name] %in% split_cand))
   conf_crit <- process_split_config(ids_candidates_left, ids_candidates_right, valid_set, alpha, lambda)
   list("node_id" = node_id, "feature" = var_name, "feature_type" = "categorical", "split_cand" = split_cand, "ids_left_child" = ids_candidates_left, "ids_right_child" = ids_candidates_right, "conf_crit" = conf_crit)
 }
@@ -123,27 +122,32 @@ process_split_config <- function(ids_left, ids_right, valid_set, alpha, lambda) 
 #' @keywords internal
 #'
 process_covariate <- function(var_name, x_data, node_id, valid_set, alpha, lambda) {
-  covariate = x_data[, colnames(x_data) == var_name]
+  covariate <- x_data[valid_set$testing_ids, colnames(x_data) == var_name]
   ## Numeric covariates:
   if (inherits(covariate, "numeric")) {
-    # All split points.
     uniques <- sort(unique(covariate))
-    # Vector of split points.
     split_candidates <- (uniques[-length(uniques)] + uniques[-1]) / 2
     split_candidates <- split_candidates[sapply(split_candidates, eval_split_cand_numeric, covariate = covariate, valid_set = valid_set, alpha = alpha)]
-    lapply(X = split_candidates, FUN = process_split_config_numeric, node_id = node_id, var_name = var_name, covariate = covariate, valid_set = valid_set, alpha = alpha, lambda = lambda)
+    lapply(X = split_candidates, FUN = process_split_config_numeric, node_id = node_id, var_name = var_name, x_data = x_data, valid_set = valid_set, alpha = alpha, lambda = lambda)
   } else {
     # Sort factor levels by mean prediction.
-    covariate_with_pred <- covariate[valid_set$testing_ids]
-    levels_sorted <- sort(tapply(valid_set$.pred, covariate_with_pred, mean))
+    levels_sorted <- sort(tapply(valid_set$.pred, covariate, mean))
     covariate_sorted <- factor(covariate, levels = names(levels_sorted))
     levels_vector <- levels(covariate_sorted)
-    # List of left-node factor levels.
-    split_candidates <- lapply(seq_along(levels_vector)[-length(levels_vector)], function(i) {
-      utils::head(levels_vector, i)
-    })
-    split_candidates <- split_candidates[sapply(split_candidates, eval_split_cand_categorical, covariate = covariate, valid_set = valid_set, alpha = alpha)]
-    lapply(X = split_candidates, FUN = process_split_config_categorical, node_id = node_id, var_name = var_name, covariate = covariate, valid_set = valid_set, alpha = alpha, lambda = lambda)
+    if (length(levels_vector) == 1) {
+      NULL
+    } else {
+      # List of left-node factor levels.
+      split_candidates <- lapply(seq_along(levels_vector)[-length(levels_vector)], function(i) {
+        utils::head(levels_vector, i)
+      })
+      if (length(split_candidates) == 0) {
+        NULL
+      } else {
+        split_candidates <- split_candidates[sapply(split_candidates, eval_split_cand_categorical, covariate = covariate, valid_set = valid_set, alpha = alpha)]
+        lapply(X = split_candidates, FUN = process_split_config_categorical, node_id = node_id, var_name = var_name, x_data = x_data, valid_set = valid_set, alpha = alpha, lambda = lambda)
+      }
+    }
   }
 }
 
