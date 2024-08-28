8e88221c01c225e5c96c1afb0072b44fad21b509
holgstr <hbj.loewe@gmail.com>
2024-08-28 21:30:06 +0200

    replace S-learner with T-learner

--- R/get_valid_set.R
+++ R/get_valid_set.R
@@ -18,20 +18,26 @@ get_valid_set <- function(data, target, learner, cv_folds, treatment = NULL) {
     training_data <- rsample::analysis(split)
     testing_data <- rsample::assessment(split)
     testing_ids <- rsample::complement(split)
-    # Fit the model on the training data.
-    model <- learner %>%
-      parsnip::fit(stats::as.formula(paste(target, "~ .")), data = training_data)
-    # Predict on the testing data.
-    if (is.null(treatment)) {
+    if (is.null(treatment)) { # Regression:
+      # Fit the model on the training data.
+      model <- learner %>%
+        parsnip::fit(stats::as.formula(paste(target, "~ .")), data = training_data)
+      # Get predictions:
       cbind(stats::predict(model, testing_data), testing_ids)
-    } else {
-      treat_levels <- levels(testing_data[[treatment]])
+    } else { # Treatment Effects:
+      treat_levels <- levels(training_data[[treatment]])
       # Treatment model predictions:
-      testing_data[[treatment]] <- treat_levels[2]
-      pred_t <- stats::predict(model, testing_data)[[1]]
+      training_treat <- training_data[training_data[[treatment]] == treat_levels[2], ]
+      training_treat[[treatment]] <- NULL
+      model_treat <- learner %>%
+        parsnip::fit(stats::as.formula(paste(target, "~ .")), data = training_treat)
+      pred_t <- stats::predict(model_treat, testing_data)[[1]]
       # Control model predictions:
-      testing_data[[treatment]] <- treat_levels[1]
-      pred_c <- stats::predict(model, testing_data)[[1]]
+      training_ctrl<- training_data[training_data[[treatment]] == treat_levels[1], ]
+      training_ctrl[[treatment]] <- NULL
+      model_ctrl <- learner %>%
+        parsnip::fit(stats::as.formula(paste(target, "~ .")), data = training_ctrl)
+      pred_c <- stats::predict(model_ctrl, testing_data)[[1]]
       # Format output:
       data.frame(pred_treat = pred_t, pred_ctrl = pred_c, testing_ids)
     }
