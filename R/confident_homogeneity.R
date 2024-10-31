c73c5c562886a68546687b710fb7e1bc9ce8aed1
holgstr <hbj.loewe@gmail.com>
2024-10-31 22:04:09 +0100

    validate weighting in gain formula

--- R/confident_homogeneity.R
+++ R/confident_homogeneity.R
@@ -52,12 +52,13 @@ avg_width <- function(valid_set, alpha) {
 total_width <- function(valid_set, ids_left, ids_right, alpha) {
   valid_set_left <- subset(valid_set, valid_set$testing_ids %in% ids_left)
   valid_set_right <- subset(valid_set, valid_set$testing_ids %in% ids_right)
+  # Lei et al. first use sums and then same divisor, so this is mathematically equivalent.
   w_left <- length(valid_set_left) / (length(valid_set_left) + length(valid_set_right))
   w_left * avg_width(valid_set_left, alpha) + (1 - w_left) * avg_width(
     valid_set_right,
     alpha
   )
-  #nrow(valid_set_left) * avg_width(valid_set_left, alpha) + nrow(valid_set_right) * avg_width(valid_set_right, alpha)
+  #avg_width(valid_set_left, alpha) + avg_width(valid_set_right, alpha)
 }
 
 #' Helper to compute the mean absolute deviation of a validation set
@@ -91,12 +92,13 @@ avg_dev <- function(valid_set, alpha) {
 total_dev <- function(valid_set, ids_left, ids_right, alpha) {
   valid_set_left <- subset(valid_set, valid_set$testing_ids %in% ids_left)
   valid_set_right <- subset(valid_set, valid_set$testing_ids %in% ids_right)
+  # Lei et al. first use sums and then same divisor, so this is mathematically equivalent.
   w_left <- length(valid_set_left) / (length(valid_set_left) + length(valid_set_right))
   w_left * avg_dev(valid_set_left, alpha) + (1 - w_left) * avg_dev(
     valid_set_right,
     alpha
   )
-  #nrow(valid_set_left) * avg_dev(valid_set_left, alpha) + nrow(valid_set_right) * avg_dev(valid_set_right, alpha)
+  #avg_dev(valid_set_left, alpha) + avg_dev(valid_set_right, alpha)
 }
 
 #' Helper to compute confident homogeneity
